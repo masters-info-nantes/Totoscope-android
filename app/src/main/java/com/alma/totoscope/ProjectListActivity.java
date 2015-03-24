c7305e6eed523ca383f7913fb2c84d2c59a6af0c
Woohyunnie <marie.lenogue@gmail.com>
2015-03-24 19:45:15 +0100

    Add choice from gallery or camera

--- app/src/main/java/com/alma/totoscope/ProjectListActivity.java
+++ app/src/main/java/com/alma/totoscope/ProjectListActivity.java
@@ -1,10 +1,15 @@
 package com.alma.totoscope;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,29 +23,63 @@ import android.widget.ArrayAdapter;
 
 public class ProjectListActivity extends Activity {
 
+    final Context context = this;
     ListView listView;
-    Button newProject;
-    Dialog sourcesDialog;
-    ImageButton gallery;
-    ImageButton camera;
+    MenuItem newProject;
+    //AlertDialog.Builder sourcesDialog;
+    //ImageButton gallery;
+    //ImageButton camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
         listView = (ListView)findViewById(R.id.list);
-        newProject = (Button) findViewById(R.id.action_newProject);
-        newProject.setOnClickListener(new View.OnClickListener() {
+        newProject = (MenuItem) findViewById(R.id.action_newProject);
+
+
+        /*newProject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               sourcesDialog = new Dialog(ProjectListActivity.this);
-               sourcesDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-               sourcesDialog.setContentView(getLayoutInflater().inflate(R.layout.sources_layout,null));
-               sourcesDialog.show();
-            }
-        });
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
-        camera = (ImageButton)findViewById(R.id.camera);
+                alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Bouton", "yes");
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Bouton", "no");
+                    }
+                });
+
+
+                //AlertDialog.Builder sourcesDialog = new AlertDialog.Builder(context);
+                //sourcesDialog.setTitle("Choix de la source");
+                /*sourcesDialog.setPositiveButton("Camera"/*R.id.camera, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent takeVid = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        startActivityForResult(takeVid, 0);
+                    }
+                });*/
+                /*sourcesDialog.setNegativeButton("Gallery"/*R.id.gallery, new DialogInterface.OnClickListener() {
+                    /*@Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent takeVid = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        startActivityForResult(takeVid, 0);
+                    }
+                });*/
+                //sourcesDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+               //sourcesDialog.setContentView(getLayoutInflater().inflate(R.layout.sources_layout,null));
+               //sourcesDialog.show();
+        //    }
+        //});
+
+        /*camera = (ImageButton)findViewById(R.id.camera);
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,7 +96,7 @@ public class ProjectListActivity extends Activity {
                 startActivityForResult(pickPhoto,1);
             }
         });
-
+        */
         String[] values = new String[]{"coucou","lol","swag","poney"};
 
 
@@ -71,6 +110,7 @@ public class ProjectListActivity extends Activity {
             }
         });
     }
+
 
 
     @Override
@@ -88,11 +128,46 @@ public class ProjectListActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_newProject) {
+            newProjectDialog();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    public void newProjectDialog() {
+        Log.i("call", "newProjectDialog");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Click", "Open Gallery");
+                        Intent pickVid = new Intent(Intent.ACTION_GET_CONTENT);
+                        pickVid.addCategory(Intent.CATEGORY_OPENABLE);
+                        pickVid.setType("video/*");
+                        startActivityForResult(pickVid, 1);
+                    }
+                })
+                .setNegativeButton("Camera",  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Click", "Open Camera");
+                        Intent takeVid = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        startActivityForResult(takeVid, 0);
+                    }
+                });
+
+        AlertDialog sourcesDialog = builder.create();
+        sourcesDialog.show();
+
+        Button gallery = sourcesDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        gallery.setCompoundDrawablesWithIntrinsicBounds(this.getResources().getDrawable(R.drawable.collection), null, null, null);
+        gallery.setText("");
+
+        Button camera = sourcesDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+        camera.setCompoundDrawablesWithIntrinsicBounds(this.getResources().getDrawable(R.drawable.camera), null, null, null);
+        camera.setText("");
+    }
 }
